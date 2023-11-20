7258fa9e5d59aa9b53617fd20f03671a367adf0c
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-20 12:04:50 +0530

    new commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+
+
+//Defining Types(Explicit Types)
+
+let userName:string = "SHakil"
+let subscribers:number = 3000_0
+let issubscribed:boolean = true
+let skills:string[] = ["js","express"]
+let conut:number[] = [1,2,3,4]
+let emptyArrau:[] = []
